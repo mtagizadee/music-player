ebcb86991e780cd3a9d63ee0767da29713788b39
mtagizadee <magatagi0@gmail.com>
2022-09-06 19:11:18 +0400

    test song

---
+++ README.md
@@ -0,0 +1,4 @@
+#Notes
+- [ ] use useRef in order to set ref to the audio tag 
+- [ ] get the song from ref.current 
+- [ ] song.play() to play the song and etc...
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,19 @@
+import { useRef } from 'react';
+
 const Home = () => {
+    const songRef = useRef();
+    const onClick = () => {
+        const song = songRef.current;
+        song.play();
+    }
+
     return (
-        <div className="center-content full-screen font-bold">
-            Home Page
+        <div className="center-content full-screen font-bold flex-col">
+            <audio ref={songRef}>
+                <source src={require('../../ost.mp3')} />
+                Your browser does not support the audio element
+            </audio>
+            <button onClick={onClick}> Play </button>
         </div>
     );
 }
